54bc8cb49b14c15f423d45de68886abca2c723e1
filipkus03 <fifipolopl@wp.pl>
2024-02-10 16:05:49 +0100

    update

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+using Microsoft.Identity.Client;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+       // public void TestMethod1()
+       // {
+         //   var calculator = new Calculator();
+
+            // Act
+       //     var result = calculator.Add(2, 3);
+
+            // Assert
+       //     Assert.Equal(5, result);
+       // }
+    }
+}
---
+++ lab3/Migrations/20240210132647_Nowe.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace lab3.Migrations
+{
+    /// <inheritdoc />
+    public partial class Nowe : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "fly",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    From = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    To = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Pilot = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_fly", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "kontrola",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Fom = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    To = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateTime = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_kontrola", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "fly");
+
+            migrationBuilder.DropTable(
+                name: "kontrola");
+        }
+    }
+}
